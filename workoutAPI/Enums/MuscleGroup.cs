0ef8e95d46a163c3291055848e060c15c9b3fa9e
OscarThroedsson <oscar.throedsson@gmail.com>
2025-09-12 23:43:39 +0200

    added muscle json and enums

---
+++ workoutAPI/Enums/MuscleGroup.cs
@@ -0,0 +1,21 @@
+namespace WorkoutAPI.Enums;
+
+public enum MuscleGroup
+{
+    Abdominals,   // Abdominales
+    Abductors,    // Abductores
+    Adductors,    // Adductores
+    Biceps,       // Musculi bicipites brachii
+    Calves,       // Triceps surae
+    Chest,        // Pectorales
+    Forearms,     // Antebrachium
+    Glutes,       // Glutei
+    Hamstrings,   // Ischiocrurales
+    Lats,         // Latissimus dorsi
+    LowerBack,    // Erector spinae (pars lumbalis)
+    MiddleBack,   // Rhomboidei / Trapezius pars media
+    Neck,         // Colli
+    Quadriceps,   // Quadriceps femoris
+    Traps,        // Trapezius
+    Triceps       // Triceps brachii
+}
