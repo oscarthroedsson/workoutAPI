91afe18df7de894db3467fcc171e9093358259a7
OscarThroedsson <oscar.throedsson@gmail.com>
2025-09-25 14:50:40 +0200

    muscles comes with bodyregion

--- workoutAPI/Mappers/BodyRegionMapper.cs
+++ workoutAPI/Mappers/BodyRegionMapper.cs
@@ -1,5 +1,6 @@
 using Supabase.Postgrest;
 using workoutAPI.Models.BodyRegions;
+using workoutAPI.Models.Muscle;
 
 namespace workoutAPI.Mappers;
 
@@ -14,7 +15,13 @@ public class BodyRegionMapper
             Name = table.Name,
             LatinName = table.LatinName,
             
-            Muscles = []
+            Muscles = table.muscle_regions.Select(m => new MuscleSmall
+            {
+                Code = m.Muscles.Code,
+                Name = m.Muscles.Name,
+                LatinName = m.Muscles.LatinName
+                
+            }).ToList()
         };
 
     }
--- workoutAPI/Models/BodyRegions/BodyRegionsDTO.cs
+++ workoutAPI/Models/BodyRegions/BodyRegionsDTO.cs
@@ -23,4 +23,39 @@ public class BodyRegionsDTO : BaseModel
 
     [Column("latinName")]
     public string LatinName { get; set; }
+    
+    // Possible relationalshops
+    [Column("muscle_regions")]
+    public List<BodyRegionMuscleRelationship>? muscle_regions { get; set; }
 }
+
+public class BodyRegionMuscleRelationship
+{
+    [PrimaryKey("id")]
+    public string Id { get; set; }
+    
+    [Column("muscle_id")]
+    public string MuscleId { get; set; }
+    
+    [Column("muscles")]
+    public BodyRegionMuscle? Muscles { get; set; }
+}
+
+public class BodyRegionMuscle
+{
+    [PrimaryKey("id")]
+    public string Id { get; set; }
+    
+    [Column("code")]
+    public string Code { get; set; }
+    
+    [Column("name")]
+    public string Name { get; set; }
+    
+    [Column("latinName")]
+    public string LatinName { get; set; }
+}
+
+
+
+
