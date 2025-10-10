afa05e315c600368a94a362b56e9cebfc55437a7
OscarThroedsson <oscar.throedsson@gmail.com>
2025-10-10 11:01:20 +0200

    implemented all new and updated logic to program.cs

--- workoutAPI/Program.cs
+++ workoutAPI/Program.cs
@@ -1,6 +1,7 @@
 using DotNetEnv;
 using workoutAPI.Extensions;
 using workoutAPI.Middlewear;
+using EasyCaching.InMemory;
 using workoutAPI.Service;
 
 Env.Load();
@@ -14,28 +15,51 @@ builder.Services.AddControllers()
             System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull;
     });
 
+builder.Services.AddEasyCaching(options =>
+{
+    options.UseInMemory(inMemoryOptions =>
+    {
+        // InMemoryOptions.DBConfig är typen InMemoryCachingOptions
+        inMemoryOptions.DBConfig = new InMemoryCachingOptions
+        {
+            ExpirationScanFrequency = 60, // städa var 60 sekunder
+            SizeLimit = 10000,
+            EnableReadDeepClone = true,
+            EnableWriteDeepClone = false
+        };
+
+        // Provider-level settings
+        inMemoryOptions.MaxRdSecond = 0;
+        inMemoryOptions.EnableLogging = true;
+    }, "default"); 
+});
+
+builder.Services.AddSingleton<StaticDataCacheService>();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSingleton<HeaderManager>();
+builder.Services.AddSingleton<QuotaService>();
+builder.Services.AddSingleton<BillingService>();
+builder.Services.AddSingleton<CacheManager>();
+
 await builder.AddSupabaseAsync();
-
-
-
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 var app = builder.Build();
 
+
+
 app.UseWhen(
     context => context.Request.Path.StartsWithSegments("/api/exercise"),
     appBuilder => appBuilder.UseExerciseMiddleware());
-
-
-
-
-
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+if (app.Environment.IsDevelopment()) app.MapOpenApi();
 
+
+using (var scope = app.Services.CreateScope())
+{
+    var cacheService = scope.ServiceProvider.GetRequiredService<StaticDataCacheService>();
+    await cacheService.PreloadCacheAsync();
+}
 
 app.MapControllers();
 app.UseHttpsRedirection();
