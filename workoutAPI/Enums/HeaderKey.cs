ef0c1e22d5a2e9e41584939215fd0f1b67a0522f
OscarThroedsson <oscar.throedsson@gmail.com>
2025-10-10 11:00:22 +0200

    added enums for headers so we have consistency

---
+++ workoutAPI/Enums/HeaderKey.cs
@@ -0,0 +1,11 @@
+namespace workoutAPI.Headers
+{
+    public enum HeaderKey
+    {
+        QuotaExceeded,
+        OveragePoints,
+        OverageCost,
+        WithinLimit,
+        PointsRequested
+    }
+}
